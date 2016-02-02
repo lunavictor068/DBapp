f2b00bb7b6cbf843b9605874658ea6a026e53663
Victor Luna <lunavictor068@gmail.com>
2016-02-01 19:52:37 -0700

    Functional version. Still have to do some very small stuff.

--- src/DBapp/Main.java
+++ src/DBapp/Main.java
@@ -5,26 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import javax.print.*;
-import javax.print.attribute.HashPrintRequestAttributeSet;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.sql.SQLException;
-
 public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         ModelData.dbConnection = new DBConnection();
         Scene root = new Scene(FXMLLoader.load(getClass().getResource("FXML/main.fxml")));
+        root.getStylesheets().add(getClass().getResource("test.css").toExternalForm());
         primaryStage.setTitle("Database Application");
         primaryStage.setScene(root);
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-                launch(args);
     }
 
 }
